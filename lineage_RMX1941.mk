67ccb97dfd8678e773536b50f87c77e35dc3f36e
devil-black-786 <fahimahmad904@gmail.com>
2020-09-28 16:21:12 -1000

    RMX1941: Cleaned up props

--- lineage_RMX1941.mk
+++ lineage_RMX1941.mk
@@ -1,3 +1,21 @@
+#
+# Copyright 2020 The Android Open Source Project
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -7,6 +25,13 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from RMX1941 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Define first api level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p_mr1.mk)
+
+# Bootanimation
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1560
 
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1941
